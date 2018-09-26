548427288730b5edcfa252d4269c625dd9fb4604
Tejas Singh <42096849+Tejassingh1010@users.noreply.github.com>
2018-09-26 22:51:31 +0530

    Add files via upload

---
+++ Assignment 9.2.R
@@ -0,0 +1,9 @@
+
+# THis is the outcome of assignment 9.2. 
+
+# 1 sampel propotions test witout continutiy correction.
+
+pnorm(1)
+prop.test(65,210, p=0.40,alternative ="two.sided",conf.level=0.95,correct=F)
+
+  
